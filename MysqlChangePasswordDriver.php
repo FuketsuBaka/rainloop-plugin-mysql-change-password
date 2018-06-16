9dad116865e92ad2fecb5c07f701c66db1aea789
FuketsuBaka <34390952+FuketsuBaka@users.noreply.github.com>
2018-06-16 20:49:02 +0700

    Fixed missed encription cases.

--- MysqlChangePasswordDriver.php
+++ MysqlChangePasswordDriver.php
@@ -54,32 +54,34 @@ class MysqlChangePasswordDriver implements \RainLoop\Providers\ChangePassword\Ch
 	{
 		$bResult = false;
 
-		$mysqli = new mysqli ($this->sConfig['host'], $this->sConfig['user'], $this->sConfig['pass'], $this->sConfig['db']);
-		$str_query_start  = "UPDATE `".$this->sConfig['table']."` ";
-		$str_query_start .= "SET `".$this->sConfig['col_pass']."`=";
+		$str_query_start  = "UPDATE `".$this->sConfig['table']."`";
+		$str_query_start .= " SET `".$this->sConfig['col_pass']."`=";
 
 		$str_query_enc    = '';
+		// Comment this switch section and drop your method in the var above, 
+		// so the option in the admin panel won't take any effect
 		switch($this->sConfig['enc_method']){
 			case 'ENCRYPT':
-				$str_query_enc = "ENCRYPT('". $sNewPassword ."') ";
+				$str_query_enc = "ENCRYPT('". $sNewPassword ."')";
 				break;
 			case 'HASH-SHA256':
-				$str_query_enc = '';
+				$str_query_enc = "'". hash ('SHA256', $sNewPassword) . "'";
 				break;
                         case 'HASH-SHA512':
-                                $str_query_enc = '';
+                                $str_query_enc = "'". hash ('SHA512', $sNewPassword) . "'";
                                 break;
                         case 'HASH-MD5':
-                                $str_query_enc = '';
+                                $str_query_enc = "'". hash ('MD5', $sNewPassword) . "'";
                                 break;
                         case 'None (For extremals)':
                                 $str_query_enc = "'" .$sNewPassword ."'";
                                 break;
 			default:
-				$str_query_enc = "ENCRYPT('". $sNewPassword ."') ";
+				$str_query_enc = "ENCRYPT('". $sNewPassword ."')";
 		}
-		$str_query_where  = "WHERE `".$this->sConfig['col_email']."`='". $oAccount->Email () ."'";
+		$str_query_where  = " WHERE `".$this->sConfig['col_email']."`='". $oAccount->Email () ."'";
 
+		$mysqli = new mysqli ($this->sConfig['host'], $this->sConfig['user'], $this->sConfig['pass'], $this->sConfig['db']);
 		$result = $mysqli->query ($str_query_start . $str_query_enc . $str_query_where);
 
 		if ($result)
